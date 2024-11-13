b11709f7ae2480d06f3e21c3e18147c985699031
matias <matiasbarrale22@gmail.com>
2024-11-13 14:12:57 -0300

    Enlaces y algunos errores

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ function seleccionarSeccion(element) {
     });
     element.classList.add('seleccionada');
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.imProyectos');
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modalImage');
+    const modalTitle = document.querySelector('.titleImage');
+    const modalText = document.querySelector('.textImage');
+    const closeBtn = document.querySelector('.close');
+
+    images.forEach(img => {
+        img.addEventListener('click', function () {
+            
+            modalImage.src = this.src;
+            modalTitle.textContent = this.getAttribute('data-title');
+            modalText.textContent = this.getAttribute('data-text');
+            
+            
+            modal.style.display = 'block';
+        });
+    });
+
+    
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
