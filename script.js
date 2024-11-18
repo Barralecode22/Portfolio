a7da3b0fe8a8625d1853d719423d160b273bd7bc
matias <matiasbarrale22@gmail.com>
2024-11-18 20:18:03 -0300

    Update-Projects

--- script.js
+++ script.js
@@ -14,31 +14,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const images = document.querySelectorAll('.imProyectos');
     const modal = document.getElementById('modal');
     const modalImage = document.getElementById('modalImage');
+    const modalIcon = document.getElementById('modalIcon');
     const modalTitle = document.querySelector('.titleImage');
     const modalText = document.querySelector('.textImage');
     const closeBtn = document.querySelector('.close');
 
     images.forEach(img => {
         img.addEventListener('click', function () {
-            
             modalImage.src = this.src;
             modalTitle.textContent = this.getAttribute('data-title');
             modalText.textContent = this.getAttribute('data-text');
             
+            modalIcon.src = this.getAttribute('data-icon') || './assets/icon-default.png';
+            modalIcon2.src = this.getAttribute('data-icon2') || './assets/icon-default2.png';
+            modalIcon3.src = this.getAttribute('data-icon3') || './assets/icon-default3.png';
             
             modal.style.display = 'block';
         });
     });
 
-    
     closeBtn.addEventListener('click', function () {
         modal.style.display = 'none';
     });
 
-    
     window.addEventListener('click', function (event) {
         if (event.target == modal) {
             modal.style.display = 'none';
         }
     });
 });
+
